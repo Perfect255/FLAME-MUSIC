fb8bb78c5f6cd3c7dd592c5259a6a1f0458d9c0f
Perfect255 <106339624+Perfect255@users.noreply.github.com>
2022-05-30 09:17:07 +0530

    Update temp.py

--- Flame/Player/temp.py
+++ Flame/Player/temp.py
@@ -14,7 +14,7 @@ import asyncio
 async def demn(flm):
     chat = "@TempMailBot"
     msg = await eor(flm, "Generating Temporary Mail...")
-    async with ultroid_bot.conversation(chat) as conv:
+    async with BOT.conversation(chat) as conv:
         try:
             response = conv.wait_event(events.NewMessage(
                 incoming=True,
@@ -26,7 +26,7 @@ async def demn(flm):
             await conv.send_message("/create")
             response = await response
             link = ((response).reply_markup.rows[2].buttons[0].url)
-            await ultroid_bot.send_read_acknowledge(chat)
+            await BOT.send_read_acknowledge(chat)
         except YouBlockedUserError:
             await msg.edit("Boss! Please Unblock @TempMailBot ")
             return
